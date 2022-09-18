3a634597b542fbddfae31d89d037359c1aaee985
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2022-09-18 08:45:44 -0600

    fix: adding cookie parser

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors"; 
 import router from "../routes";
 import deserializeUser from "../middlewares/deserializeUser";
+import * as cookieParser from "cookie-parser";
 
 function createServer()  {
     const app = express();
 
     app.use(express.json());
+    app.use(cookieParser());
     app.use(cors({
         origin: ['http://127.0.0.1:5173'],
         credentials: true 
