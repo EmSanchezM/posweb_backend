6d4b98758d3d66efe115f087e38d423004c8bb42
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2022-09-18 08:34:39 -0600

    fix: cors available for frontend

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -7,7 +7,10 @@ function createServer()  {
     const app = express();
 
     app.use(express.json());
-    app.use(cors());
+    app.use(cors({
+        origin: ['http://127.0.0.1:5173'],
+        credentials: true 
+    }));
     app.use(deserializeUser); 
 
     app.use(router);
