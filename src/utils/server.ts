fee8af0a32056626fb3ae21553f42b2e49be0e9e
Marvin Toro <ingricardotoro@gmail.com>
2022-12-03 21:02:58 -0600

    test cookieParse

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,23 +1,29 @@
-import express from "express";
-import cors from "cors"; 
-import router from "../routes";
-import deserializeUser from "../middlewares/deserializeUser";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import cors from 'cors';
+import router from '../routes';
+import deserializeUser from '../middlewares/deserializeUser';
+import cookieParser from 'cookie-parser';
 
-function createServer()  {
-    const app = express();
+function createServer() {
+  const app = express();
 
-    app.use(express.json());
-    app.use(cookieParser());
-    app.use(cors({
-        origin: ['http://127.0.0.1:5173', 'https://dataplus-posweb.netlify.app'],
-        credentials: true 
-    }));
-    app.use(deserializeUser); 
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(
+    cors({
+      origin: [
+        'http://127.0.0.1:5173',
+        'https://dataplus-posweb.netlify.app',
+        'http://localhost:5173',
+      ],
+      credentials: true,
+    })
+  );
+  app.use(deserializeUser);
 
-    app.use(router);
+  app.use(router);
 
-    return app; 
+  return app;
 }
 
 export default createServer;
